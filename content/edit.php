2fe18b72f3922c54712374dfff234c3a4457359b
MarinaAparicioSantos <marina.aparicio.santos.al@iespoligonosur.org>
2021-11-19 12:41:45 +0100

    edit

--- content/edit.php
+++ content/edit.php
@@ -198,31 +198,31 @@
         <input type="text" name="origen" value="<?php ?>" placeholder="Ciudad de origen"/>
         <span style="color:red;"></span>
 
-        <br>
+        <br><br>
 
         <label for="destino">Ciudad de destino: </label>
         <input type="text" name="destino" value="<?php ?>" placeholder="Ciudad de destino"/>
         <span style="color:red;"></span>
 
-        <br>
+        <br><br>
 
         <label for="operadora">Operadora: </label>
         <input type="text" name="operadora" value="<?php ?>" placeholder="Operadora"/>
         <span style="color:red;"></span>
 
-        <br>
+        <br><br>
 
         <label for="fecha">Fecha del viaje: </label>
         <input type="date" name="estreno" value="<?php ?>" placeholder="Fecha del viaje">
         <span style="color:red;"></span>
 
-        <br>
+        <br><br>
 
         <label for="CantidadViajeros">Cantidad de viajeros: </label>
         <input type="number" name="CantidadViajeros" value="<?php ?>" placeholder="Cantidad de viajeros">
         <span style="color:red;"></span>
 
-        <br>
+        <br><br>
 
         <input type="submit" value="Editar" class="btn-enviar">
 
