23be90e0e36bd04557a67bb015f60dabfcfce264
MarinaAparicioSantos <marina.aparicio.santos.al@iespoligonosur.org>
2021-11-19 09:58:40 +0100

    edit

--- content/edit.php
+++ content/edit.php
@@ -1,12 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Editar vuelo</title>
 </head>
+
 <body>
-    
+
+
+    <form>
+
+        <label for="origen">Ciudad de origen: </label>
+        <input type="text" name="origen" value="" placeholder="Ciudad de origen"/>
+
+        <br>
+
+        <label for="destino">Ciudad de destino: </label>
+        <input type="text" name="destino" value="" placeholder="Ciudad de destino"/>
+
+        <br>
+
+        <label for="operadora">Operadora: </label>
+        <input type="text" name="operadora" value="" placeholder="Operadora"/>
+
+        <br>
+
+        <label for="fecha">Fecha del viaje: </label>
+        <input type="date" name="estreno" placeholder="Fecha del viaje">
+
+        <br>
+
+        <label for="CantidadViajeros">Cantidad de viajeros: </label>
+        <input type="number" name="CantidadViajeros" placeholder="CantidadViajeros">
+
+    </form>
+
 </body>
+
 </html>
