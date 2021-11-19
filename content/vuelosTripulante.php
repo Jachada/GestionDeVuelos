f7b4f54e9ef34460e3b087df6861af3a6d2267ce
MarinaAparicioSantos <marina.aparicio.santos.al@iespoligonosur.org>
2021-11-19 09:31:22 +0100

    vuelosTripulante

--- content/vuelosTripulante.php
+++ content/vuelosTripulante.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
+    <table border=1>
+
+        <thead>
+
+            <tr>
+
+                <th>Ciudad de origen</th>
+                <th>Ciudad de destino</th>
+                <th>Operadora</th>
+                <th>Fecha</th>
+                <th>Cantidad de viajeros</th>
+
+            </tr>
+        </thead>
+
+        <tbody>
+
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+        </tbody>
+
+    </table>
+
 </body>
+
 </html>
