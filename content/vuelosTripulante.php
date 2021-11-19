46de6435927c281df45b39f4d26019a8cccef84e
Farricius <jose.hernandez.chacon.al@iespoligonosur.org>
2021-11-19 10:35:38 +0100

    vuelosTripulante muestra la tabla, falta que funcione con una SESIÓN DE X TRIPULACIÓN

--- content/vuelosTripulante.php
+++ content/vuelosTripulante.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Vuelos Tripulante</title>
+    <?php
+    include "databaseManager.inc.php";
+    ?>
 </head>
 <body>
     <table border=1>
@@ -18,11 +21,25 @@
             </tr>
         </thead>
         <tbody>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <?php 
+            
+            $companya = "Vueling";
+            $printeaTodo = mostrarPorCompanya($companya);
+
+            foreach ($printeaTodo as $vuelo) {
+            //$varid = $vuelo["id"]; ignore
+            echo "<tr>";
+            echo "<td>$vuelo[CiudadOrigen]</td>";
+            echo "<td>$vuelo[CiudadDestino]</td>";
+            echo "<td>$vuelo[Operadora]</td>";
+            echo "<td>$vuelo[Fecha]</td>";
+            echo "<td>$vuelo[CantidadViajeros]</td>";
+            //echo "<td><a href='edit.php?id=$varid'>Editar</td>";
+            echo "</tr>";
+            }
+            ?>
+
+
         </tbody>
     </table>
 </body>
