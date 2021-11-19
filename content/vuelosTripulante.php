f697dfa01ecfbff875ed3e7c0165c0f988d6fa20
MarinaAparicioSantos <marina.aparicio.santos.al@iespoligonosur.org>
2021-11-19 12:08:18 +0100

    vuelos tripulante

--- content/vuelosTripulante.php
+++ content/vuelosTripulante.php
@@ -6,12 +6,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vuelos Tripulante</title>
+    <link rel="stylesheet" href="../style/bootstrap.min.css"> 
+    <link rel="stylesheet" href="../style/style.css">
     <?php
     include "databaseManager.inc.php";
     ?>
 </head>
 
 <body>
+
+<div class="container-fluid">
+      <div class="row">
+
+        <header class="cabecera col-12 ">
+          <div class="menu_bar d-flex justify-content-center align-content-center">
+            <a href="#" class="bt-menu"><img src="../images/logo.png" alt="" class=""><i class="fas fa-bars"></i></a>
+          </div>
+       
+          <nav class="menu">
+            <ul class="d-flex d-inline-block justify-content-around align-items-center">
+              <li>
+                <a href="../index.php"><i class=""></i><span><strong>Inicio</strong></span></a>
+              </li>
+              
+              <li class="">
+                <a href="acercade.php"><i class=""></i><span><strong>Acerca de</strong></span></a>
+              </li>
+              <div class="div-logo"> <li> <a href="index.php"><img src="../images/logo.png" alt="" class="logo"></a></li></div>
+             
+              <li class="">
+                <a href="login.php"><i class=""></i><span><strong>Login</strong></span></a>
+              </li>
+              <li class="">
+                <a href="./content/registro.php"><i class=""></i><span><strong>Registro</strong></span></a>
+              </li>
+              
+            </ul>
+          </nav>
+        </header>
+      </div>
+      
+      
+    </div>
+
     <table border=1>
         <thead>
             <tr>
@@ -35,7 +72,7 @@
             echo "<td>$vuelo[Operadora]</td>";
             echo "<td>$vuelo[Fecha]</td>";
             echo "<td>$vuelo[CantidadViajeros]</td>";
-            //echo "<td><a href='edit.php?id=$varid'>Editar</td>";
+            echo "<td><a href='edit.php?id=$varid'>Editar</td>";
             echo "</tr>";
             }
             ?>
